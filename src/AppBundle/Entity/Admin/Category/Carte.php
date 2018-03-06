494abda755ef9d947071a0932e87dea49da58c18
Hubsine <contact@hubsine.com>
2018-03-06 14:17:33 +0100

    - add new property on carte entity and form

--- src/AppBundle/Entity/Admin/Category/Carte.php
+++ src/AppBundle/Entity/Admin/Category/Carte.php
@@ -28,6 +28,26 @@ class Carte implements AdminEntityInterface
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
+    
+    /**
+     * @var integer
+     * 
+     * @ORM\Column(type="integer", name="price")
+     * 
+     * @Assert\NotBlank(message="assert.not_blank")
+     * @Assert\Type(type="integer", message="assert.type")
+     */
+    private $price;
+    
+    /**
+     * @var string
+     * 
+     * @ORM\Column(name="about", type="string")
+     * 
+     * @Assert\NotBlank(message="assert.not_blank")
+     * @Assert\Type(type="string", message="assert.type")
+     */
+    private $about;
 
     public static function getRoutePrefix(): string
     {
@@ -43,5 +63,52 @@ class Carte implements AdminEntityInterface
     {
         return $this->id;
     }
-}
 
+    /**
+     * Set price
+     *
+     * @param integer $price
+     *
+     * @return Carte
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return integer
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set about
+     *
+     * @param string $about
+     *
+     * @return Carte
+     */
+    public function setAbout($about)
+    {
+        $this->about = $about;
+
+        return $this;
+    }
+
+    /**
+     * Get about
+     *
+     * @return string
+     */
+    public function getAbout()
+    {
+        return $this->about;
+    }
+}
--- src/AppBundle/Form/Type/Admin/Category/CarteType.php
+++ src/AppBundle/Form/Type/Admin/Category/CarteType.php
@@ -6,6 +6,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class CarteType extends AbstractType
 {
@@ -14,9 +16,17 @@ class CarteType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name', TextType::class, array(
-            'label'     => 'form.category.name.carte'
-        ));
+        $builder
+            ->add('name', TextType::class, array(
+                'label'     => 'form.category.name.carte'
+            ))
+            ->add('price', IntegerType::class, array(
+                'label' => 'form.price',
+            ))
+            ->add('about', TextareaType::class, array(
+                'label' => 'form.about'
+            ))
+        ;
     }
     
     /**
