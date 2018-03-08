8b132ce590dd2b8ae70d13e847f545468beb1d8a
Hubsine <contact@hubsine.com>
2018-03-08 23:06:41 +0100

    - create UndefinedConstantException

---
+++ src/AppBundle/Exception/UndefinedConstantException.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Exception;
+
+use AppBundle\Exception\ExceptionTrait;
+
+/**
+ * Description of BadInstanceException
+ *
+ * @author Hubsine <contact@hubsine.com>
+ */
+class UndefinedConstantException  extends \Exception
+{
+    use ExceptionTrait;
+    
+    const DEFAULT_MESSAGE = 'Undefined constant exception "%s"';
+    
+    /**
+     * 
+     * @param string $constantName 
+     * @param string $message
+     * @param int $code
+     * @param \Throwable $previous
+     */
+    public function __construct($constantName, string $message = "", int $code = 0, \Throwable $previous = null) 
+    {
+        if( empty($message) )
+        {
+            $message    = sprintf( self::DEFAULT_MESSAGE, $constantName );
+        }
+        
+        parent::__construct($message, $code, $previous);
+    }
+}
