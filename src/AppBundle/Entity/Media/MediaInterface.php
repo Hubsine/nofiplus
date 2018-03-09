42bd221fab67ebc3b7dcd8edffb96ccb9455ab3b
Hubsine <contact@hubsine.com>
2018-03-09 16:38:12 +0100

    - remove imlement interface admin

--- src/AppBundle/Entity/Media/MediaInterface.php
+++ src/AppBundle/Entity/Media/MediaInterface.php
@@ -2,14 +2,12 @@
 
 namespace AppBundle\Entity\Media;
 
-use AppBundle\Entity\AdminEntityInterface;
-
 /**
  * Description of MediaInterface
  *
  * @author Hubsine <contact@hubsine.com>
  */
-interface MediaInterface extends AdminEntityInterface
+interface MediaInterface 
 {
     /**
      * @return string a file path
