84e6fede932f33e51d561da2ab8a6f26ce5828d2
Hubsine <contact@hubsine.com>
2018-04-12 19:32:26 +0200

    - capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,29 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.10.1"
 
-set :application, "nofi_plus"
-set :repo_url, "ssh://nofiplus@nofiplus.com/var/www/vhosts/nofiplus.com/git/nofiplus.com"
-set :branch, 'prod'
-set :stage, :prod
-set :ssh_user, 'root'
-server '94.23.206.21', user: fetch(:ssh_user), roles: %w{web app db}
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/var/www/vhosts/nofiplus.com/httpdocs/NOFIPLUS"
+
+set :application, "nofi_plus"
+set :repo_url, 'git@github.com:Hubsine/nofiplus.git'
+
+set :file_permissions_users, ["nginx"]
+set :file_permissions_paths, ["var", "web/uploads"]
+
+set :log_level, :debug
+
+#set :linked_files, ["app/config/parameters.yml"]
+#set :linked_dirs, ["var/logs"]
+
+### 
+# Symfony 
+###
+
+###
+# Composer
+###
+
+set :composer_install_flags, '--no-dev --no-interaction --quiet --optimize-autoloader --ignore-platform-reqs --no-scripts'
+
+###
+# Events
+###
