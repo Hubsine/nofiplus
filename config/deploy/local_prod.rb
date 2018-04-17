9d07772fa2d480d9b6c6d7fe490f08c927599f8f
Hubsine <contact@hubsine.com>
2018-04-17 16:46:40 +0200

    - fixed composer element version

--- config/deploy/local_prod.rb
+++ config/deploy/local_prod.rb
@@ -1,7 +1,7 @@
 #/usr/local/var/www/nofiplus/.git
 set :repo_url, '/usr/local/var/www/nofiplus/.git'
-set :branch, ENV.fetch("BRANCH", "local_prod")
-#set :branch, 'local_prod'
+#set :branch, ENV.fetch("BRANCH", "local_prod")
+set :branch, 'local_prod'
 set :stage, :staging
 set :deploy_to, "/usr/local/var/www/NOFIPLUS_PROD"
 
