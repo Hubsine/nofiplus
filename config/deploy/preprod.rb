eca7051976ea70bcd6f3fb822d3dfc29bd25fc3b
Hubsine <contact@hubsine.com>
2018-04-17 14:51:51 +0200

    - update capi config

--- config/deploy/preprod.rb
+++ config/deploy/preprod.rb
@@ -3,7 +3,8 @@ set :repo_url, 'git@github.com:Hubsine/nofiplus.git'
 
 set :symfony_env,  "preprod"
 
-set :branch, 'master'
+set :branch, 'preprod'
+#set :stage, :preprod
 set :ssh_user, 'nofiplus'
 server '94.23.206.21', user: fetch(:ssh_user), roles: %w{web app db}
 
