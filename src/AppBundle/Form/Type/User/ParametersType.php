926f0c006e119cfa311bb41d7aab9ab5b6d6483a
Hubsine <contact@hubsine.com>
2019-10-03 22:47:44 +0200

    Fix "Attempted to load class "LegacyFormHelper" from namespace "FOS\UserBundle\Util"."

--- src/AppBundle/Form/Type/User/ParametersType.php
+++ src/AppBundle/Form/Type/User/ParametersType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use FOS\UserBundle\Util\LegacyFormHelper;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
@@ -26,7 +27,7 @@ abstract class ParametersType extends AbstractType
             $constraintsOptions['groups'] = array(reset($options['validation_groups']));
         }
 
-        $builder->add('current_password', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\PasswordType'), array(
+        $builder->add('current_password', PasswordType::class, array(
             'label' => 'form.current_password',
             'translation_domain' => 'FOSUserBundle',
             'mapped' => false,
@@ -36,8 +37,8 @@ abstract class ParametersType extends AbstractType
             ),
         ));
 
-        $builder->add('plainPassword', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\RepeatedType'), array(
-            'type' => LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\PasswordType'),
+        $builder->add('plainPassword', RepeatedType::class, array(
+            'type' => PasswordType::class,
             'options' => array('translation_domain' => 'FOSUserBundle'),
             'first_options' => array('label' => 'form.new_password'),
             'second_options' => array('label' => 'form.new_password_confirmation'),
